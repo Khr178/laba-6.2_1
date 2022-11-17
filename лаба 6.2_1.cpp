0b54e50628b86f6d980ece5452eeb0c99bf6574b
Khrystyna <hrustunakopeika2005@gmail.com>
2022-11-17 15:29:56 +0200

    first master commit

---
+++ лаба 6.2_1.cpp
@@ -0,0 +1,30 @@
+﻿// лаба 6.2_1.cpp 
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "\nEnter the size of the array(max 10)\n";
+    const int n = 10;
+    int a[n] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    
+
+    cout << "Array before modfication:" << endl;
+    print_array(a, n);
+
+    reverse(a, n);
+
+    cout << "\nArray after modfication:" << endl;
+    print_array(a, n);
+    cout << endl;
+
+    return 0;
+}
+
+
+
+
---
+++ юніт тест 6.2.1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <cassert>
+#include <iostream>
+#include "../лаба 6.2_1/лаба 6.2_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест621
+{
+	TEST_CLASS(юніттест621)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a;
+			a = main();
+			Assert::AreEqual(a, 0);
+		}
+	};
+}
